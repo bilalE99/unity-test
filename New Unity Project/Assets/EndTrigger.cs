2b90ca79e3508f672d0d075d62ae53da6c59e62c
bilalE99 <bilalelfitouri1999@gmail.com>
2019-11-19 15:39:05 +0000

    Add files

---
+++ New Unity Project/Assets/EndTrigger.cs
@@ -0,0 +1,12 @@
+﻿
+using UnityEngine;
+
+public class EndTrigger : MonoBehaviour
+{
+    public GameManager gameManager1;
+
+    public void OnTriggerEnd()
+    {
+        gameManager1.completeLevel();
+    }
+}
---
+++ New Unity Project/Assets/Scripts/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    bool gameEnd = false;
+    public float endDelay = 1f;
+
+    public void completeLevel()
+    {
+        Debug.Log("won");
+    }
+    public void EndGame()
+    {
+        if (gameEnd == false)
+        {
+            gameEnd = true;
+            Invoke("Restart", endDelay); //invoke calls a delay before method is called
+        }
+    }
+
+    void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
